88eac4c16830172c867621f10095c0d3926b808b
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2018-10-15 12:52:53 +1100

    Added Top and Bottom in normalization

--- Haskell/propositional-logic/src/Eval.hs
+++ Haskell/propositional-logic/src/Eval.hs
@@ -5,6 +5,8 @@ import Ast
 
 {- transform implication and bi implication in terms of And, Or and Not -}
 normalize :: PExpr -> PExpr
+normalize Top = Top
+normalize Bottom = Bottom
 normalize (Var p) = Var p
 normalize (Not p) = Not (normalize p)
 normalize (Or p q) = Or (normalize p) (normalize q)
@@ -16,12 +18,18 @@ normalize (Bi p q) = And (normalize (Imp p q)) (normalize (Imp q p))
 
 {- https://en.wikipedia.org/wiki/Negation_normal_form -} 
 nnf :: PExpr -> PExpr
-nnf (Not (Or p q)) = And (nnf (Not p)) (nnf (Not q))
-nnf (Not (And p q)) = Or (nnf (Not p)) (nnf (Not q))
+nnf Top = Top
+nnf Bottom = Bottom
+nnf (Var p) = Var p
+nnf (Not Top) = Bottom
+nnf (Not Bottom) = Top
+nnf (Not (Var p)) = Not (Var p)
 nnf (Not (Not p)) = nnf p
+nnf (Not (And p q)) = Or (nnf (Not p)) (nnf (Not q))
+nnf (Not (Or p q)) = And (nnf (Not p)) (nnf (Not q))
 nnf (And p q) = And (nnf p) (nnf q) 
 nnf (Or p q) = Or (nnf p) (nnf q)
-nnf e = e
+
 
 normalizennf :: PExpr -> PExpr
 normalizennf = nnf . normalize
